248a6bbc29bcc00ab844e0264e59aa92bd69ce55
Hannah Lee <dwingule@gmail.com>
2020-03-15 17:01:49 -0400

    adding s_date and e_date

--- naver_crawler.py
+++ naver_crawler.py
@@ -14,18 +14,27 @@ import datetime
 from wordcloud import WordCloud
 
 
-def crawler(n, query):
+def crawler(n, query, s_date=None, e_date=None):
     """
     returns n number of url's of news articles given a search query [query], sorted by relevance
     """
     page = 1
     num_articles = 0
     url_list = []
+    if s_date is not None or e_date is not None:
+        s_from = s_date.replace(".","")
+        e_to = e_date.replace(".","")
 
     pbar = tqdm(total=n, initial=num_articles)
 
     while num_articles < n:
-        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&start=" + str(page)
+        if s_date is None and e_date is None:
+            url = "https://search.naver.com/search.naver?where=news&query=" +\
+            query + "&start=" + str(page)
+        else:
+            url = "https://search.naver.com/search.naver?where=news&query=" + query +\
+            "&sort=0&ds=" + s_date + "&de=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" +\
+            s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
         request = requests.get(url)
         content = request.content
         soup = BeautifulSoup(content, 'html.parser')
@@ -90,13 +99,19 @@ def get_freq(wordlist):
     result.sort(reverse = True)
     return result
 
-def main(n, query):
+def main(n, query, s_date=None, e_date=None):
     """
     ### Parameters:
     [n] int
         - specifies the number of articles to crawl
     [query] string
         - search query
+    [s_date] string
+        - in format xxxx.xx.xx, specifying the starting
+        year, month and date
+    [e_date] string
+        - in format xxxx.xx.xx, specifying the ending
+        year, month and date
 
     ### Note:
     n = number of articles in [result] + len(failed_urls)
@@ -105,7 +120,7 @@ def main(n, query):
     # query = input("Input the search query: ")
     # txt = input("filename: ")
 
-    url_list = crawler(int(n), query) # creates a list of url's
+    url_list = crawler(int(n), query, s_date, e_date) # creates a list of url's
     result = ""
     failed_urls = []
 
@@ -120,7 +135,11 @@ def main(n, query):
     # saving news article text and failed urls to txt files
     # assumes the folder `results` and its subfolder `failed`
     dt = datetime.datetime.now()
-    filename = str(n) + '_'+ query + '_' + dt.strftime("%m%d%y_%H%M")
+    if s_date is None and e_date is None:
+        filename = str(n) + '_'+ query + '_' + dt.strftime("%m%d%y_%H%M")
+    else:
+        filename = str(n) + '_'+ query + '_' + dt.strftime("%m%d%y_%H%M") +\
+                    "_" + s_date + 'to' + e_date
 
     with open('results/news_text/' + filename + ".txt", "w") as f:
         f.write(result)
@@ -142,4 +161,5 @@ def main(n, query):
 
 
 # To run:
-main(3, '유투브')
+main(3, '페이스북', s_date = "2019.01.01", e_date = "2019.05.01")
+main(3, '페이스북')
